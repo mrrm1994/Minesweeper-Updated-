93f86525bbccd1e3e5b27d9bd775ceba859744a4
Franmanuel5 <Francisco.valentin5@upr.edu>
2017-03-20 14:37:05 -0400

    Change bomb array name.

--- src/RandomBombGenerator.java
+++ src/RandomBombGenerator.java
@@ -2,21 +2,21 @@ import java.util.Random;
 
 public class RandomBombGenerator { 
 
-	public void bombGenerator(int bomb[][]){
+	public void bombGenerator(int bombBox[][]){
 		Random generator = new Random(); 
 		int i = 0; 
 		while(i<10){
-			if(bomb[generator.nextInt(9)+1][generator.nextInt(9)+1] != 1){
-				bomb[generator.nextInt(9)+1][generator.nextInt(9)+1] = 1;
+			if(bombBox[generator.nextInt(9)+1][generator.nextInt(9)+1] != 1){
+				bombBox[generator.nextInt(9)+1][generator.nextInt(9)+1] = 1;
 				i++;
 			}
 		}
 	}
 	
-	public void showBombs(int bomb[][]){
+	public void showBombs(int bombBox[][]){
 		for(int i = 0 ; i<10 ; i++){
 			for(int j = 0 ; j<10 ; j++){
-				System.out.print(bomb[j][i]);
+				System.out.print(bombBox[j][i]);
 			}
 			System.out.println();
 		}
