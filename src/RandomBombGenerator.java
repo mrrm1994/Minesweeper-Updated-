bae872658225317c863b5962bc5ec5cc4e77b584
Franmanuel5 <Francisco.valentin5@upr.edu>
2017-03-21 22:23:04 -0400

    Nothing Changed

--- src/RandomBombGenerator.java
+++ src/RandomBombGenerator.java
@@ -17,8 +17,8 @@ public class RandomBombGenerator {
 	}
 
 	public void showBombsConsole(int bombBox[][]){  
-		for(int i = 0 ; i<10 ; i++){
-			for(int j = 0 ; j<10 ; j++){
+		for(int i = 1 ; i<10 ; i++){
+			for(int j = 1 ; j<10 ; j++){
 				System.out.print(bombBox[j][i]);
 			}
 			System.out.println();
